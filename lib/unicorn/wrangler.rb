6b5dcab9d22580973c1028b758b2a2434e758411
Tom Ward <tom@popdog.net>
2013-06-14 17:35:53 +0100

    Remember which unicorn pid we are monitoring

--- lib/unicorn/wrangler.rb
+++ lib/unicorn/wrangler.rb
@@ -3,11 +3,13 @@ require "unicorn/wrangler/version"
 module Unicorn
   class Wrangler
     attr_reader :command, :pidfile, :grace_period
+    attr_accessor :unicorn_pid
 
     def initialize(command, options = {})
       @command = command
       @pidfile = File.expand_path(options[:pidfile] || 'unicorn.pid')
       @grace_period = options[:grace_period] || 60
+      @verbose = options[:verbose]
     end
 
     def start
@@ -17,15 +19,27 @@ module Unicorn
         exit
       end
 
-      if unicorn_running?
+      if old_unicorn = old_pidfile_contents && process_running?(old_unicorn)
+        signal :QUIT, old_unicorn
+      end
+
+      self.unicorn_pid = pidfile_contents
+
+      if unicorn_pid && process_running?(unicorn_pid)
+        debug "running unicorn found at #{unicorn_pid}"
         restart_unicorn
       else
-        Process.spawn(command, pgroup: true)
-        wait_for { unicorn_running? }
+        debug "no running unicorn found, starting new instance"
+        self.unicorn_pid = Process.spawn(command, pgroup: true)
+        wait_for { process_running?(unicorn_pid) }
+        debug "new instance started with PID #{unicorn_pid}"
       end
 
       loop do
-        exit unless unicorn_running?
+        unless process_running?(unicorn_pid)
+          debug "unicorn(#{unicorn_pid}) no longer running, quitting."
+          exit
+        end
         sleep 1
       end
     end
@@ -35,26 +49,38 @@ module Unicorn
     def trap_signals(*signals, &block)
       signals.map(&:to_s).each do |signal|
         trap(signal) do
-          puts "unicorn-wrangler #{Process.pid} received #{signal} (managing #{unicorn_pid})"
+          debug "received #{signal} (managing #{unicorn_pid})"
           block.call signal
         end
       end
     end
 
     def restart_unicorn
-      original_pid = unicorn_pid
-      Process.kill "USR2", unicorn_pid
-      wait_for { unicorn_pid != original_pid }
+      debug "restarting unicorn process at #{unicorn_pid}"
+      signal "USR2", unicorn_pid
+      wait_for { pidfile_contents && unicorn_pid != pidfile_contents }
+      debug "new master process started at #{pidfile_contents}"
       sleep grace_period
-      Process.kill "TERM", original_pid
+      signal "TERM", unicorn_pid
+      self.unicorn_pid = pidfile_contents
     end
 
-    def unicorn_pid
+    def pidfile_contents
       File.exist?(pidfile) && File.read(pidfile).to_i
     end
 
-    def unicorn_running?
-      unicorn_pid && Process.getpgid(unicorn_pid)
+    def old_pidfile_contents
+      File.exist?(pidfile + ".oldbin") && File.read(pidfile + ".oldbin").to_i
+    end
+
+    def signal(name, pid)
+      debug "signalling #{pid} with #{name}"
+      Process.kill name, pid
+    rescue Errno::ESRCH
+    end
+
+    def process_running?(pid)
+      pid && Process.getpgid(pid)
     rescue Errno::ESRCH
       false
     end
@@ -66,14 +92,18 @@ module Unicorn
     end
 
     def kill_unicorn_after_delay
-      if unicorn_running?
-        puts "Preparing to kill unicorn in #{grace_period} seconds"
+      if process_running?(unicorn_pid)
         unless fork
+          debug "preparing to kill unicorn #{unicorn_pid} in #{grace_period} seconds"
           Process.setsid
           sleep grace_period
-          Process.kill :TERM, unicorn_pid if unicorn_running?
+          signal :TERM, unicorn_pid if process_running?(unicorn_pid)
         end
       end
+    end
+
+    def debug(message)
+      puts message if @verbose
     end
   end
 end
